3c1e2f98d43060c77fc0f28bb8576932ac7c01a4
Dmitry Goryunov <d.f.goryunov@gmail.com>
2022-02-27 20:54:16 +0100

    Delete greetings

--- main.py
+++ main.py
@@ -1,49 +1,101 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler
+import logging
 
-import requests
-import telegram
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.utils.helpers import effective_message_type
+
 import os
+
+APP_NAME = os.environ.get('APP_NAME', 'telegram-bot-help-in-berlin')
+PORT = int(os.environ.get('PORT', 5000))
 TOKEN = os.environ["TOKEN"]
+REMINDER_MESSAGE = """
+    Dear group members,
+    We remind you that this is a group for helping and finding help. If you can – write in English. Please refrain from putting unrelated entries here. Anybody adding hate or trolling comments will be banned from the group.
+    The overview of useful links  about documents, transport, accommodation etc. can be found here https://bit.ly/help-for-ukrainians
 
-def callback_alarm(bot, job):
-    bot.send_message(chat_id=job.context, text='Alarm')
+    Уважаемые участники группы,
+    Напоминаем вам, что цель этой группы – предоставление и поиск помощи. Если можете – пожалуйста пишите по английски. Пожалуйста, воздержитесь от размещения здесь записей, не имеющих отношения к теме. Пользователи, добавляющие агрессивные или тролящие комментарии,  будут удаляны из группы.
+    Обзор полезных ссылок о документах, транспорте, жилье и т.д. можно найти здесь
+     https://bit.ly/help-for-ukrainians
 
-def callback_timer(bot, update, job_queue):
+    Шановні учасники групи
+    Нагадуємо вам, що це група для допомоги та пошуку допомоги. Якщо можете – пишіть англійською. Будь ласка, утримайтеся від розміщення записів, що не мають відношення до теми. Будь-хто, хто додаватиме коментарі ненависті або тролінгу, буде видалено з групи.
+    Огляд корисних посилань про документи, транспорт, житло тощо. можна знайти тут
+     https://bit.ly/help-for-ukrainians
+"""
+ONE_HOUR = 60 * 60
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def help_command(bot: Bot, update: Update) -> None:
+    """Send a message when the command /help is issued."""
+    bot.send_message(chat_id=update.message.chat_id, text=REMINDER_MESSAGE)
+
+
+def handle_msg(bot: Bot, update: Update) -> None:
+    """Echo the user message."""
+    tp = effective_message_type(update.message)
+    logger.info(f'Handling type is {tp}')
+    if effective_message_type(update.message) in ['new_chat_members', 'left_chat_member']:
+        bot.delete_message(chat_id=update.message.chat_id,
+                           message_id=update.message.message_id)
+
+
+def callback_alarm(bot: Bot, job):
+    """callback_alarm"""
+    logger.info('Sending a reminder')
+    bot.send_message(chat_id=job.context, text=REMINDER_MESSAGE)
+
+
+def callback_timer(bot: Bot, update, job_queue):
+    """callback_timer"""
     bot.send_message(chat_id=update.message.chat_id,
-                      text='Starting!')
-    job_queue.run_repeating(callback_alarm, 5, context=update.message.chat_id)
+                     text='Starting!')
+    job_queue.run_repeating(callback_alarm, ONE_HOUR,
+                            context=update.message.chat_id)
 
-def stop_timer(bot, update, job_queue):
+
+def stop_timer(bot: Bot, update, job_queue):
+    """stop_timer"""
     bot.send_message(chat_id=update.message.chat_id,
-                      text='Stoped!')
+                     text='Stoped!')
     job_queue.stop()
 
-updater = Updater(TOKEN)
-updater.dispatcher.add_handler(CommandHandler('start', callback_timer, pass_job_queue=True))
-updater.dispatcher.add_handler(CommandHandler('stop', stop_timer, pass_job_queue=True))
 
-updater.start_polling()
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(TOKEN)
 
-def webhook(request):
-    bot = telegram.Bot(token=TOKEN)
-    if request.method == "POST":
-        update = Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat.id
-        reminder_message = """
-                    Dear group members,
-                    We remind you that this is a group for helping and finding help. Please refrain from putting unrelated entries here. Anybody adding hate or trolling comments will be banned from the group.
-                    The overview of useful links  about documents, transport, accommodation etc. can be found here https://bit.ly/help-for-ukrainians
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
 
-                    Уважаемые участники группы,
-                    Напоминаем вам, что цель этой группы – предоставление и поиск помощи. Пожалуйста, воздержитесь от размещения здесь записей, не имеющих отношения к теме. Пользователи, добавляющие агрессивные или тролящие комментарии,  будут удаляны из группы.
-                    Обзор полезных ссылок о документах, транспорте, жилье и т.д. можно найти здесь
-                     https://bit.ly/help-for-ukrainians
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler(
+        'start', callback_timer, pass_job_queue=True))
+    dispatcher.add_handler(CommandHandler(
+        'stop', stop_timer, pass_job_queue=True))
+    dispatcher.add_handler(CommandHandler('help', help_command))
 
-                    Шановні учасники групи
-                    Нагадуємо вам, що це група для допомоги та пошуку допомоги. Будь ласка, утримайтеся від розміщення записів, що не мають відношення до теми. Будь-хто, хто додаватиме коментарі ненависті або тролінгу, буде видалено з групи.
-                    Огляд корисних посилань про документи, транспорт, житло тощо. можна знайти тут
-                     https://bit.ly/help-for-ukrainians
-        """
-        bot.sendMessage(chat_id=chat_id, text=reminder_message)
-    return "OK"
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.all, handle_msg))
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook(f'https://{APP_NAME}.herokuapp.com/{TOKEN}')
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
