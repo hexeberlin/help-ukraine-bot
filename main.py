fc09037652fe44743c856dbc579ad2dfe3f13a9b
Dmitry Goryunov <d.f.goryunov@gmail.com>
2022-02-27 21:05:46 +0000

    Remove main

--- main.py
+++ main.py
@@ -62,33 +62,44 @@ def stop_timer(bot, update, job_queue):
     job_queue.stop()
 
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater(TOKEN)
+updater = Updater(TOKEN)
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler(
-        'start', callback_timer, pass_job_queue=True))
-    dispatcher.add_handler(CommandHandler(
-        'stop', stop_timer, pass_job_queue=True))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+# on different commands - answer in Telegram
+dispatcher.add_handler(CommandHandler(
+    'start', callback_timer, pass_job_queue=True))
+dispatcher.add_handler(CommandHandler(
+    'stop', stop_timer, pass_job_queue=True))
+dispatcher.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, handle_msg))
+# on non command i.e message - echo the message on Telegram
+dispatcher.add_handler(MessageHandler(
+    Filters.text & ~Filters.command, handle_msg))
 
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+# Start the Bot
+updater.start_polling()
 
 
-if __name__ == '__main__':
-    main()
+def webhook(request):
+    # bot = telegram.Bot(token=TOKEN)
+    if request.method == "POST":
+        logger.info('Got message')
+        # update = Update.de_json(request.get_json(force=True), bot)
+        # chat_id = update.message.chat.id
+        # reminder_message = """
+        #             Dear group members,
+        #             We remind you that this is a group for helping and finding help. Please refrain from putting unrelated entries here. Anybody adding hate or trolling comments will be banned from the group.
+        #             The overview of useful links  about documents, transport, accommodation etc. can be found here https://bit.ly/help-for-ukrainians
+        #             Уважаемые участники группы,
+        #             Напоминаем вам, что цель этой группы – предоставление и поиск помощи. Пожалуйста, воздержитесь от размещения здесь записей, не имеющих отношения к теме. Пользователи, добавляющие агрессивные или тролящие комментарии,  будут удаляны из группы.
+        #             Обзор полезных ссылок о документах, транспорте, жилье и т.д. можно найти здесь
+        #              https://bit.ly/help-for-ukrainians
+        #             Шановні учасники групи
+        #             Нагадуємо вам, що це група для допомоги та пошуку допомоги. Будь ласка, утримайтеся від розміщення записів, що не мають відношення до теми. Будь-хто, хто додаватиме коментарі ненависті або тролінгу, буде видалено з групи.
+        #             Огляд корисних посилань про документи, транспорт, житло тощо. можна знайти тут
+        #              https://bit.ly/help-for-ukrainians
+        # """
+        # bot.sendMessage(chat_id=chat_id, text=reminder_message)
+    return "OK"
