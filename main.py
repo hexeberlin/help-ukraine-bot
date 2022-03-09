33ab088a0271fdd16693dff19fe4c1cb1c049033
elinamaas <elina.zarisheva@gmail.com>
2022-03-09 18:26:36 +0100

    add cities command

--- main.py
+++ main.py
@@ -175,9 +175,9 @@ def find_replies(bot: Bot, update: Update) -> None:
 def cities_command(bot: Bot, update: Update, name=None):
     logger.info("cities")
 
-    # book = guidebook.load_guidebook()
-    # results = commands.cities(book, name)
-    results = "heloo world"
+    book = guidebook.load_guidebook()
+    results = commands.cities(book, name)
+    # results = "heloo world"
     bot.send_message(chat_id=update.message.chat_id, text=results)
 
 
