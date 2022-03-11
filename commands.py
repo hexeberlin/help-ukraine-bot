c971432cdfe4fe4d50bdc5e5e84a4f0a5bab53c9
elinamaas <elina.zarisheva@gmail.com>
2022-03-11 15:05:22 +0100

    add commnand to help

--- commands.py
+++ commands.py
@@ -3,7 +3,6 @@ import os
 import guidebook
 import knowledge
 
-
 HRYVNIA_TITLE = "Обмен гривен/Exchange hryvnia"
 LEGAL_TITLE = "Юридическая помощь/Legal help"
 TEACHERS_TITLE = "Онлайн уроки для детей/Online lessons for children"
@@ -25,22 +24,27 @@ def countries(book, name=None):
 
 def help():
     return ("Привет! Используйте одну из команд:\n"
-           + "/cities чтобы увидеть все чаты помощи по городам Германии\n"
-           + "/countries чтобы увидеть все чаты помощи по странам\n"
-           + "/hryvnia чтобы получить информацию про обмен гривен\n"
-           + "/legal чтобы получить юридическую помощь\n"
-           + "/evacuation общие чаты об эвакуации из Украины\n"
-           + "/evacuationCities чаты эвакуации по городам\n"
-           + "/childrenLessons онлайн уроки для детей из Украины\n\n"
-           + os.linesep
-           + "Hi! Please use one of the following commands:\n"
-           + "/cities to display existing chats for cities in Germany\n"
-           + "/countries to display existing chats for the countries\n"
-           + "/hryvnia to display information about hryvnia exchange\n"
-           + "/legal to get information about legal help\n"
-           + "/evacuation to get information about evacuation from Ukraine\n"
-           + "/evacuationCities evacuation chats for cities\n"
-           + "/childrenLessons online lessons for children from Ukraine")
+            + "/cities чтобы увидеть все чаты помощи по городам Германии\n"
+            + "/countries чтобы увидеть все чаты помощи по странам\n"
+            + "/hryvnia чтобы получить информацию про обмен гривен\n"
+            + "/legal чтобы получить юридическую помощь\n"
+            + "/evacuation общие чаты об эвакуации из Украины\n"
+            + "/evacuationCities чаты эвакуации по городам\n"
+            + "/childrenLessons онлайн уроки для детей из Украины\n"
+            + "/handbook Методичка\n"
+            + "\n"
+
+            + os.linesep
+            + "Hi! Please use one of the following commands:\n"
+            + "/cities to display existing chats for cities in Germany\n"
+            + "/countries to display existing chats for the countries\n"
+            + "/hryvnia to display information about hryvnia exchange\n"
+            + "/legal to get information about legal help\n"
+            + "/evacuation to get information about evacuation from Ukraine\n"
+            + "/evacuationCities evacuation chats for cities\n"
+            + "/childrenLessons online lessons for children from Ukraine\n"
+            + "/handbook Handbook\n"
+            + "\n")
 
 
 def hryvnia():
