a4a48b869c88a92094484dd050e45343e04747f7
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2022-08-15 17:31:11 +0200

    Remove banking

--- src/finance.py
+++ src/finance.py
@@ -7,10 +7,6 @@ from src.common import send_results
 from src.guidebook import NameType
 
 
-def banking_command(bot: Bot, update: Update):
-    send_results(bot, update, group_name=NameType.banking, name=None)
-
-
 def hryvnia_command(bot: Bot, update: Update):
     send_results(bot, update, group_name=NameType.hryvnia, name=None)
 
@@ -24,13 +20,11 @@ def job_center_calc_command(bot: Bot, update: Update):
 
 
 def register_commands(dispatcher: Dispatcher) -> List[BotCommand]:
-    dispatcher.add_handler(CommandHandler("banking", banking_command))
     dispatcher.add_handler(CommandHandler("hryvnia", hryvnia_command))
     dispatcher.add_handler(CommandHandler("job_center_calc", job_center_calc_command))
     dispatcher.add_handler(CommandHandler("schufa", schufa_command))
 
     return [
-        BotCommand("banking", "Где открыть банковский счёт"),
         BotCommand("hryvnia", "Обменять гривны"),
         BotCommand("job_center_calc", "Расчёт пособия от JobCenter при наличии зарплаты"),
         BotCommand("schufa", "Как получить справку Schufa"),
