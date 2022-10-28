1d4b11b705629b8866551b1ee12f6dd1e15a897d
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2022-10-28 13:48:12 +0200

    Remove hryvnia command

--- src/finance.py
+++ src/finance.py
@@ -7,10 +7,6 @@ from src.common import send_results
 from src.guidebook import NameType
 
 
-def hryvnia_command(bot: Bot, update: Update):
-    send_results(bot, update, group_name=NameType.hryvnia, name=None)
-
-
 def schufa_command(bot: Bot, update: Update):
     send_results(bot, update, group_name=NameType.schufa, name=None)
     
@@ -20,12 +16,10 @@ def job_center_calc_command(bot: Bot, update: Update):
 
 
 def register_commands(dispatcher: Dispatcher) -> List[BotCommand]:
-    dispatcher.add_handler(CommandHandler("hryvnia", hryvnia_command))
     dispatcher.add_handler(CommandHandler("job_center_calc", job_center_calc_command))
     dispatcher.add_handler(CommandHandler("schufa", schufa_command))
 
     return [
-        BotCommand("hryvnia", "Обменять гривны"),
         BotCommand("job_center_calc", "Расчёт пособия от JobCenter при наличии зарплаты"),
         BotCommand("schufa", "Как получить справку Schufa"),
     ]
