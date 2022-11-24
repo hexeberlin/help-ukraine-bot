2c649df4b94b7e58630be97b89947ebd6ec243d4
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2022-11-24 14:04:30 +0100

    Combine both disabled commands

--- src/medical.py
+++ src/medical.py
@@ -16,10 +16,6 @@ def psychological_command(bot: Bot, update: Update):
     send_results(bot, update, group_name=NameType.psychological, name=None)
 
 
-def kids_with_special_needs_command(bot: Bot, update: Update):
-    send_results(bot, update, group_name=NameType.disabled, name="Помощь для детей с особыми потребностями")
-
-
 def disabled_command(bot: Bot, update: Update):
     send_results(bot, update, group_name=NameType.disabled, name=None)
 
@@ -29,9 +25,6 @@ def pregnant_command(bot: Bot, update: Update):
 
 
 def register_commands(dispatcher: Dispatcher) -> List[BotCommand]:
-    dispatcher.add_handler(
-        CommandHandler("kids_with_special_needs", kids_with_special_needs_command)
-    )
     dispatcher.add_handler(CommandHandler("medical", medical_command))
     dispatcher.add_handler(CommandHandler("psychological", psychological_command))
     dispatcher.add_handler(CommandHandler("disabled", disabled_command))
@@ -39,7 +32,6 @@ def register_commands(dispatcher: Dispatcher) -> List[BotCommand]:
 
     return [BotCommand("medical", "Медицинская помощь"),
             BotCommand("psychological", "Психологическая помощь"),
-            BotCommand("kids_with_special_needs", "Помощь для детей с особыми потребностями"),
             BotCommand("disabled", "Помощь для людей с особыми потребностями"),
             BotCommand("pregnant", "Помощь для беременных"),
             ]
