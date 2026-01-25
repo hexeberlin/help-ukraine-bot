841992238c55e6922f2b17623d8c03949c347741
Grigory Bordyugov <gregbg@gmail.com>
2026-01-25 20:05:52 +0100

    Remove class NameType (#183)

--- src/guidebook.py
+++ src/guidebook.py
@@ -1,65 +1,11 @@
 """Getting information from knowledge base in guidebook.yaml"""
 
 import logging
-from enum import Enum
 from typing import Any, Dict, List, Optional
 
 from yaml import safe_load
 
 logger = logging.getLogger(__name__)
-
-
-class NameType(str, Enum):
-    accommodation: str = "accommodation"
-    animal_help: str = "animals"
-    apartments: str = "apartments"
-    apartment_approval: str = "apartment_approval"
-    beauty: str = "beauty"
-    beschwerde: str = "beschwerde"
-    cities: str = "cities"
-    countries: str = "countries"
-    deutschlandticket: str = "deutschlandticket"
-    diplom: str = "diplom"
-    education: str = "education"
-    entertainment: str = "entertainment"
-    evacuation: str = "evacuation"
-    evacuation_cities: str = "evacuation_cities"
-    food: str = "food"
-    free_stuff: str = "free_stuff"
-    furniture: str = "furniture"
-    general_information: str = "general_information"
-    german: str = "deutsch"
-    handicap: str = "handicap"
-    job_center_calc: str = "job_center_calc"
-    jobs: str = "jobs"
-    kindergeld: str = "kindergeld"
-    leave: str = "leave"
-    legal: str = "legal"
-    medical: str = "medical"
-    minors: str = "minors"
-    no_ads: str = "no_ads"
-    passport: str = "passport"
-    lost_passport: str = "lost_passport"
-    photo: str = "photo"
-    pregnant: str = "pregnant"
-    psychological: str = "psychological"
-    qrcode: str = "qrcode"
-    return_to_ukraine: str = "return_to_ukraine"
-    rundfunk: str = "rundfunk"
-    school: str = "school"
-    schufa: str = "schufa"
-    search: str = "search"
-    simcards: str = "simcards"
-    social_help: str = "social_help"
-    statements: str = "official statements"
-    taxis: str = "taxis"
-    telegram_translation: str = "telegram_translation"
-    translators: str = "translators"
-    transport: str = "transport"
-    vaccination: str = "vaccination"
-    volunteer: str = "volunteer"
-    university: str = "university"
-    wbs: str = "wbs"
 
 
 class Guidebook:
@@ -131,24 +77,24 @@ class Guidebook:
         )
 
     def get_results(self, group_name: str, name: str = None) -> str:
-        return self.get_info(group_name=NameType[group_name], name=name)
+        return self.get_info(group_name=group_name, name=name)
 
-    def get_cities(self, group_name: Enum = NameType.cities, name: str = None) -> str:
+    def get_cities(self, group_name: str = "cities", name: str = None) -> str:
         if not name:
             return self.format_results(
                 "Пожалуйста, уточните название города: /cities Name\n"
             )
         if name in self.vocabulary:
             return self.get_info(
-                group_name=group_name.value, name=self.vocabulary.get(name)
+                group_name=group_name, name=self.vocabulary.get(name)
             )
-        return self.get_info(group_name=group_name.value, name=name)
+        return self.get_info(group_name=group_name, name=name)
 
     def get_countries(
-        self, group_name: Enum = NameType.countries, name: Optional[str] = None
+        self, group_name: str = "countries", name: Optional[str] = None
     ) -> str:
         if not name:
             return self.format_results(
                 "Пожалуйста, уточните название страны: /countries Name\n"
             )
-        return self.get_info(group_name=group_name.value, name=name)
+        return self.get_info(group_name=group_name, name=name)
